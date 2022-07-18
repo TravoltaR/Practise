1ef5df6423b52f65c0c631972ec30178473c88e7
Sergejs Muromcevs <sergej.muromcev@gmail.com>
2022-07-18 12:23:47 +0300

    Code review comments

--- MD5_rožnovs.cpp
+++ MD5_rožnovs.cpp
@@ -45,6 +45,11 @@ int main()
     return 0;
 }
 
-
-
-
+//TODO: check code review (and remove notes once done):
+// Comments
+// * English is the language of choice
+// * Excessive/obvious comments are usually considered as a bad practice, code should be readable itself, only business or technically tricky moments to be commented
+// ** (Reference to tasks sub-points are ok, as can be considered business-valuable info)
+// Default value of date commonly referred to Unix time (https://en.wikipedia.org/wiki/Unix_time) is 00:00:00 UTC Thursday, 1 January 1970
+// getDate() returning day only - shady, but might be required by task
+// Documentation - highly recommended to have Readme.md file nearby with complete task description
