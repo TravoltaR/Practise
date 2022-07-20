248d1d47d9772099faa56e638419a34b09367102
Aleksandrs <74254195+TravoltaR@users.noreply.github.com>
2022-07-21 01:51:46 +0300

    Update MD5_rožnovs.cpp

--- MD5_rožnovs.cpp
+++ MD5_rožnovs.cpp
@@ -1,4 +1,5 @@
-#include <iostream>
+##include <iostream>
+#include <string>
 using namespace std;
 
 class Date
@@ -9,8 +10,14 @@ private: ////Deklarējam klases objekta mainīgos "diena, mēnesis, gads".
     int year;
 
 public:
-    //UZDEVUMA 1.1 punkts
-    Date(); //noklusejuma konstruktors
+//UZDEVUMA 1.1 punkts
+    Date()
+    {
+        day = 1;
+        month = 1;
+        year = 1970;
+    }
+
     Date(int dayValue, int monthValue, int yearValue) //konstruktors ar parametriem
     {
         day = dayValue;
@@ -18,32 +25,88 @@ public:
         year = yearValue;
     }
 
-    //UZDEVUMA 1.1 punkts
+//UZDEVUMA 1.1 punkts
     void setDate(int dayValue, int monthValue, int yearValue) // datuma iestatisana
     {
         day = dayValue;
         month = monthValue;
         year = yearValue;
     }
-    int getDate() //datuma noskaidrošana
+    string getDate() //datuma noskaidrošana
     {
-        return day;
+        return std::to_string(day) + "." + std::to_string(month) +  "." +  std::to_string(year);
     }
     void print()
     {
-        cout << "Date: "  << endl;
+        cout << "Date: "  << getDate() << endl;
     }
-};
+
 
 //UZDEVUMA 1.2 punkts
+
+    int getInDays()
+    {
+        int monthsInYear = 12;
+        int daysInMonth = 30;
+
+        return   this->day +
+                (this->month - 1)*daysInMonth +
+                (this->year - 1)*monthsInYear*daysInMonth;      //month/year - 1 = январь не считается за 30 дней, не прошел еще январь. год не прошел - 1.
+    }
+
+
+    int daysBetween(Date* other) //функиця с именем daysBetween принемает в себя параметр с именем others типа Date* и возвращает значение типа int.
+    {
+        return abs(this->getInDays() - other->getInDays()); //abs - дает положительное число.
+    }
+
+
+//UZDEVUMA 1.3 punkts
+
+      Date* dateUpdate()
+    {
+
+        if (month)
+        {
+            if (day >= 30)
+            {
+            // Go to next month
+            ++month, day=1;
+            }
+            else
+            {
+                ++day;
+            }
+        }
+
+        Date* newDate = new Date(this->day, this->month, this->year); //объекты, класса и параметры перечитать.
+        return newDate;
+    }
+
+
+};
+
+    //UZDEVUMA 1.2 punkts
 int main()
 {
-    Date a;
-    a.print();
+    Date* a = new Date();    //date*  - tip peremennoj(ukazatel na objekt klassa "DATE", a - imja peremennoj, new - videlenie pamati i sozdanie novogo objekta klassa, (date()
+    Date* b = new Date(29,1,2022);
+    a->print();
+    b->print();
 
+    cout << "Days Between" <<endl;
+    //cout << a->daysBetween(a) <<endl;
+    cout << a->daysBetween(b) <<endl;   // this = a, other = b;
 
+    cout << "Next Date";
+
+    Date* c = b->dateUpdate();  cout<<endl;
+    c->print();
+    //b->print();
     return 0;
 }
+
+
 
 //TODO: check code review (and remove notes once done):
 // Comments
